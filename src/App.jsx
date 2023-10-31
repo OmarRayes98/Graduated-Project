087dea129ae5920643883389c464e1911680fc34
OmarRayes98 <omarrayes1998@gmail.com>
2023-10-31 15:16:31 +0200

    10/30/2023 first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './assets/global.css';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Button variant="primary">welcome to React bootstrap with ReduxToolKit</Button>{' '}
+      </div>
+
+    </>
+  )
+}
+
+export default App
